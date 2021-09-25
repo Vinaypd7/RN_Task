e188a809117532dacdaf6e8e894735d4802ab34b
Vinayaka P D <vinaypd37@gmail.com>
2021-09-25 00:30:07 -0700

    API Integration

--- App.js
+++ App.js
@@ -2,13 +2,29 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './src/screens/Home';
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
+
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
     <View style={{flex:1}}>
-      <Home/>
+      <Navigation/>
       <StatusBar style="auto" />
     </View>
+  );
+}
+
+export function Navigation() {
+  return (
+ 
+      <NavigationContainer independent={true}>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen  name="Home" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
+   
   );
 }
 
