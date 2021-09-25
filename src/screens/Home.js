c2490432c9a605faffa5e662fc80136e19a326f0
Vinayaka P D <vinaypd37@gmail.com>
2021-09-25 00:33:20 -0700

    API integration

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -3,8 +3,26 @@ import { StyleSheet, Text, View,ScrollView,TextInput,FlatList,ActivityIndicator,
 import {Ionicons} from '@expo/vector-icons'
 import HomeCard from '../components/HomeCard'
 import Constant from 'expo-constants'
+import { useNavigation } from '@react-navigation/native';
 
-const Home = ()=>{
+
+const Home = (navigation)=>{
+    const [value,setValue] = useState("")
+    const [HomeCardData,setHomeCard] = useState([])
+    // const dispatch = useDispatch()
+    // const HomeCardData = useSelector(state=>{
+    //     return state.cardData
+    // })
+    const [loading,setLoading] = useState(false)
+    const fetchData = () =>{
+        setLoading(true)
+        fetch(`https://api.stackexchange.com/2.3/questions?pagesize=20&order=desc&sort=activity&tagged=${value}&site=stackoverflow`)
+        .then(res=>res.json())
+        .then(data=>{
+            setLoading(false)
+            setHomeCard(data.items)
+        })
+    }
   return(
       <View style={{
           flex:1,
@@ -24,14 +42,33 @@ const Home = ()=>{
                  width:"70%",
                  backgroundColor:"#e6e6e6"
                 }}
+                value={value}
+                onChangeText={(text)=>setValue(text)}
              />
              <Ionicons
               style={{color:"#e6e6e6"}}
              name="md-search"
              size={32}
+             onPress={()=>fetchData()}
              />
           </View>
-            <HomeCard/> 
+          {loading ?<ActivityIndicator style={{marginTop:10}} size="large" color="red"/>:null } 
+          <FlatList
+           data={HomeCardData}
+           renderItem={({item})=>{
+               return <HomeCard
+                answer={item.link}
+                question={item.title}
+                tags={item.tags}
+                ansCount={item.answer_count}
+                votes={item.score}
+                name={item.owner.display_name}
+                profile={item.owner.link}
+                time={item.creation_date}
+               />
+           }}
+           keyExtractor={item=>item.question_id}
+          /> 
       </View>
   )
 }
